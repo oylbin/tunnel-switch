defdf681ddcca4de241763800dd7e64f16aa8cd8
oylbin <oylbin@gmail.com>
2012-02-29 11:14:17 +0800

    first commit

---
+++ README.mk
@@ -0,0 +1,10 @@
+### tunnel-switch is chrome extension.
+
+You can use it to cycle your proxy setting in following modes:
+
+1. no proxy
+1. use pac file proxy
+1. use ssh tunnel proxy
+
+Switchy is good, but the auto switch mode seems not work for me.
+So I created tunnel-switch.
